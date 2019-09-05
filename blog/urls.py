88aba8b199739015d65b5145439715dc75457a1b
Josguther1234 <josguther@mesoamericana.edu.gt>
2019-09-04 18:25:43 -0600

    Se agrego CSS boostrap Se creo una base para plantillas Se creo la vista en detalle de una publicacion

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.listar_pub,name='listar_pub')
+    path('',views.listar_pub,name='listar_pub'),
+    path('pub/<int:pk>/', views.detalle_pub, name='detalle_pub')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render ,get_object_or_404
 from django.utils import timezone
 from .models import Publicacion
+
 # Create your views here.
 def listar_pub(request):
     pubs = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pubs': pubs})
+
+def detalle_pub(request,pk):
+    p = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/detalle_pub.html', {'p': p})
